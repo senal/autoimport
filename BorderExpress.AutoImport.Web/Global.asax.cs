22e7cb95e3dcd97a08e83ca96c10b0166cf386a0
Manoj Prasad <kamprasad007@gmail.com>
2015-02-16 18:22:03 +0530

    AI-1 Login screen re-design and development in .NET MVC

--- BorderExpress.AutoImport.Web/Global.asax.cs
+++ BorderExpress.AutoImport.Web/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MvcApplication3;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace BorderExpress.AutoImport.Web
@@ -12,7 +14,13 @@ namespace BorderExpress.AutoImport.Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
         }
     }
 }
