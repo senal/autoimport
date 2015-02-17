114ce1c402dcea96f4ba6c8935414a0d1275ee37
Ranga Fernando <rsf@exilesoft.com>
2015-02-17 12:03:18 +0530

    AI-90 Create repositories for setup configurations
    Reference Dapper (micro ORM) to the Dal project.
    Introduced repository for user. to support the log-in functionality.
    Couldn't complete method implementation.

---
+++ BorderExpress.AutoImport.Dal/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorderExpress.AutoImport.Dal.Interfaces
+{
+    public interface IRepository<T>
+        where T: class, new()
+    {
+        IList<T> GetAll();
+        IQueryable<T> GetQueryable();
+    }
+}
---
+++ BorderExpress.AutoImport.Dal/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BorderExpress.AutoImport.Dal.Interfaces;
+using BorderExpress.AutoImport.Dal.Model;
+using BorderExpress.AutoImport.Common.Interfaces;
+using Dapper;
+
+namespace BorderExpress.AutoImport.Dal
+{
+    public class UserRepository:IUserRepository
+    {
+        private readonly IConfiguration _configuration;
+        
+        public UserRepository(IConfiguration configuration)
+        {
+            if(_configuration==null)
+                throw new ArgumentNullException("configuration");
+            _configuration = configuration;
+        }
+
+        public IList<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<User> GetQueryable()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
