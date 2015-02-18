4bb5ee04bdfdf21d523055e821769e87a87cc483
Manoj Prasad <kamprasad007@gmail.com>
2015-02-18 17:05:30 +0530

    applied style for login screen

--- BorderExpress.AutoImport.Web/App_Start/BundleConfig.cs
+++ BorderExpress.AutoImport.Web/App_Start/BundleConfig.cs
@@ -14,12 +14,17 @@ namespace MvcApplication3
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
