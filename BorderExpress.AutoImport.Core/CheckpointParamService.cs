9348bb8e96348e482a9717bd032e0a9345437084
Ranga Fernando <rsf@exilesoft.com>
2015-02-22 22:16:27 +0530

    Add service and repository classes for Checkpoint parameters.

---
+++ BorderExpress.AutoImport.Core/CheckpointParamService.cs
@@ -0,0 +1,13 @@
+﻿using BorderExpress.AutoImport.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorderExpress.AutoImport.Core
+{
+    public class CheckpointParamService :ICheckpointParamService
+    {
+    }
+}
---
+++ BorderExpress.AutoImport.Dal/CheckpointParamRepository.cs
@@ -0,0 +1,13 @@
+﻿using BorderExpress.AutoImport.Dal.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorderExpress.AutoImport.Dal
+{
+    public class CheckpointParamRepository :ICheckpointParamRepository
+    {
+    }
+}
